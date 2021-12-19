f2a99759dc7e0209f6bbb340f8137ae921edf0cf
김예리 <dpfl0445@gmail.com>
2021-12-19 08:07:58 +0000

    Update index.js

--- back/index.js
+++ back/index.js
@@ -11,9 +11,6 @@ const schema = mongoose.Schema;
 // app.set("view engine", "ejs");
 
 require("dotenv").config();
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", process.env.DB_PASS);
 
 app.use(morgan("dev"));
 // app.use("/", require("./routes/image"));
@@ -32,8 +29,8 @@ app.get("/", (req, res) => {
 // });
 
 app.listen(port, () => {
-    var dir = "./uploadedFiles";
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    // var dir = "./uploadedFiles";
+    // if (!fs.existsSync(dir)) fs.mkdirSync(dir);
     console.log("Server is working : PORT - ", port);
 });
 
