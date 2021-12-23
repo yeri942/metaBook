eb5c5955180f6bd10df36c933f1723c17b7e1abd
정병욱 <korjby400@gmail.com>
2021-12-23 16:16:00 +0900

    get comment bug fix

--- back/routes/com_router.js
+++ back/routes/com_router.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { Post, User } = require("../models");
 
-router.post("/:postId", async (req, res) => {
+// middlewares
+const loginRequired = require("../middlewares/login-required");
+
+router.post("/:postId", loginRequired, async (req, res) => {
     const { postId } = req.params;
     const { content } = req.body;
     const author = req.user.id;
@@ -16,18 +19,21 @@ router.post("/:postId", async (req, res) => {
                 $push: { comments: { content, author } },
             }
         );
-        res.json({ ok: true, message: "댓글 성공", postId, author });
+        res.json({ ok: true, message: "댓글 추가 성공", postId, author });
     } catch (err) {
-        res.json({ ok: false, message: "댓글 실패", err });
+        res.json({ ok: false, message: "댓글 추가 실패", err });
     }
 });
 
 router.get("/:postId", async (req, res) => {
     const { postId } = req.params;
-    const post = await Post.findOne({ postId });
-
-    await User.populate(post.comments, { path: "author", select: "name" });
-    res.json(post.comments);
+    const post = await Post.findOne({ _id: postId });
+    try {
+        await User.populate(post.comments, { path: "author", select: "name" });
+        res.json(post.comments);
+    } catch (err) {
+        res.json({ ok: false, message: "댓글 읽기 실패", err });
+    }
 });
 
 module.exports = router;
