fb5b6d33548481714be5a5e53412d8499dd3222e
Eungwang <eungwang1203@gmail.com>
2021-12-25 12:29:29 +0900

    'commit'

--- front/js/modal.js
+++ front/js/modal.js
@@ -19,6 +19,7 @@ window.addEventListener("DOMContentLoaded", async () => {
             console.log(res.data);
 
             const { author, content, title, thumbnailUrl, metaUrl, likeCount, likes } = res.data.post;
+            console.log(author);
             console.log(likes.includes("61c35dfce7e0d57cfd1a7cb1"));
             modal(metaUrl, title, content, thumbnailUrl, author, objectId, likeCount, likes);
             $("html, body").addClass("not_scroll");
